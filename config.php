8764e65cbbfed34fab289b5d7f183734cb119cbe
Oronya Innocent <oronyai@gmail.com>
2026-07-29 21:42:43 +0300

    Reconfiguration for localhost

--- config.php
+++ config.php
@@ -1,23 +1,10 @@
 <?php
 
-$host = 'sql312.infinityfree.com';
-$port = 3306;
-$db   = 'if0_42473575_businessdb';
-$user = 'if0_42473575';
-$pass = 'Ruth@0002';
-
-$conn = new mysqli(
-    $host,
-    $user,
-    $pass,
-    $db,
-    (int)$port
-);
+$conn =mysqli_connect("localhost","root","Ruth@0002","businessdb");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$conn->set_charset("utf8mb4");
 
 ?>
